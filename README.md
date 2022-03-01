4029d6047859c3d15f5a9b61ca7a9d320056eacc
lumontec <lmontech@orw-lmonte-7560.mgc.mentorg.com>
2022-02-28 16:31:25 -0800

    Refactor base

---
+++ README.md
@@ -0,0 +1,17 @@
+# Run in developement
+
+Install parceljs
+```
+yarn add --dev parcel
+```
+
+Run locally
+```
+yarn start
+```
+
+Build for production
+```
+yarn build
+```
+
--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-console.log('hello world')
+import './main.css'
+import curDot from './cursor-dot.js'
+
+const $ = s => document.querySelector(s)
+
+const cursor = curDot({
+	easing: 4
+})
+
+cursor.over('.react1', {
+	borderColor: 'rgba(255,255,255,.38)'
+})
+
+cursor.over('.react2', {
+	scale: .5,
+	background: '#fff'
+})
+
+cursor.over($('.react3'), {
+	scale: 1.4,
+	background: '#fff',
+	borderColor: 'transparent'
+})
